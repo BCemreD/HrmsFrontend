b9723e2ac021a1e00178729fdbc1b2adc2f887c3
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-06-30 18:23:25 +0300

    https://github.com/BCemreD/HrmsFront.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import Dashboard from './layout/Dashboard';
+import { Button, Segment } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className="main">
+     <Dashboard/>
+     </Button>
     </div>
   );
 }
---
+++ src/layout/Dashboard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Button, Segment } from 'semantic-ui-react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Segment inverted>
+                <Button inverted>Standard</Button>
+                <Button inverted color='red'>
+                    Red
+                </Button>
+                <Button inverted color='orange'>
+                    Orange
+                </Button>
+                <Button inverted color='yellow'>
+                    Yellow
+                </Button>
+                <Button inverted color='olive'>
+                    Olive
+                </Button>
+                <Button inverted color='green'>
+                    Green
+                </Button>
+                <Button inverted color='teal'>
+                    Teal
+                </Button>
+                <Button inverted color='blue'>
+                    Blue
+                </Button>
+                <Button inverted color='violet'>
+                    Violet
+                </Button>
+                <Button inverted color='purple'>
+                    Purple
+                </Button>
+                <Button inverted color='pink'>
+                    Pink
+                </Button>
+                <Button inverted color='brown'>
+                    Brown
+                </Button>
+                <Button inverted color='grey'>
+                    Grey
+                </Button>
+                <Button inverted color='black'>
+                    Black
+                </Button>
+            </Segment>
+        </div>
+    )
+}
